b9c729241f5f2f794f192dc81b78095069565748
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-05-17 17:02:40 -0400

    Use search term to retrieve and set up data

--- js/main.js
+++ js/main.js
@@ -1,11 +1,68 @@
+// ------------------------
+//  Model
+// ------------------------
+var data = {
 
-var searchTerm = 'programming';
+	results: {
+		searchTerm: null,
+		titles: null,
+		descriptions: null,
+		urls: null
+	}
 
-$.ajax({
-	dataType: "jsonp",
-	url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm,
-	success: function(data) {
-		console.log(data);
-		}
-	});
+}; // end of data
+
+
+
+// ------------------------
+//  Controler
+// ------------------------
+var controller= {
+
+	init: function(){
+		view.init();
+	},
+
+	search: function(searchTerm){
+		$.ajax({
+			dataType: "jsonp",
+			url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm,
+			success: function(results) {
+				controller.setResults(results);
+			}
+		});
+	},
+
+	setResults: function(results){
+		data.results.searchTerm = results[0];
+		data.results.titles = results[1];
+		data.results.descriptions = results[2];
+		data.results.urls = results[3];
+	}
+
+}; // end of controller
+
+
+
+// ------------------------
+//  View
+// ------------------------
+var view = {
+
+	init: function(){
+		this.createSearchHandler();
+	},
+
+	createSearchHandler: function(){
+		$("#search").on("click",function(){
+			var searchTerm = $("#searchTerm").val();
+			controller.search(searchTerm);
+		});
+	}
+
+}; // end of view
+
+
+
+controller.init();
 
