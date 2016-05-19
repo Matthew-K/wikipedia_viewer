7d64428619898de00b2bc67d65d254abd3c0ecf1
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-05-19 15:42:38 -0400

    Display search results

--- js/main.js
+++ js/main.js
@@ -28,8 +28,9 @@ var controller= {
 			dataType: "jsonp",
 			url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm,
 			success: function(results) {
-				console.log(results);
+				// console.log(results);
 				controller.setResults(results);
+				view.displayResults(controller.getResults());
 			}
 		});
 	},
@@ -39,6 +40,10 @@ var controller= {
 		data.results.titles = results[1];
 		data.results.descriptions = results[2];
 		data.results.urls = results[3];
+	},
+
+	getResults: function(){
+		return data.results;
 	}
 
 }; // end of controller
@@ -55,16 +60,36 @@ var view = {
 	},
 
 	createSearchHandler: function(){
-
 		$("#searchButton").on("click",function(){
+			// find the search term
 			var searchTerm = $("#searchTerm").val();
+			// use as as an input for an ajax call
 			controller.search(searchTerm);
+			// removes previous search results
+			$(".result").remove();
 		});
+	},
+
+	displayResults: function(results){
+		//for every title in results
+		for(var i = 0; i < results.titles.length; i++){
+			var title = results.titles[i];
+			var description = results.descriptions[i];
+			var url = results.urls[i];	
+			// append a div with the corresponding title, description, and url to #results
+			$("#results").append(
+				"<div class='result'>" + 
+					"<h2>" + title + "</h2>" + 
+					"<p>" + description + "</p>" + 
+					"<a href='" + results.urls[i] + "'>Click here for more</a>" + 
+				"</div>"
+			);
+		}
 	}
 
 }; // end of view
 
 
-
+// Initialize on start
 controller.init();
 
