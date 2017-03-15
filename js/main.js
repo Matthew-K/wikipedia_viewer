1c2bb6211720d9385a911a564efc7fe340459c4d
Matthew Kolodziejczak <makolodzi@gmail.com>
2017-03-15 14:04:29 -0400

    Remove key

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ var controller= {
 	search: function(searchTerm){
 		$.ajax({
 			dataType: "jsonp",
-			url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm,
+			url: "" + searchTerm,
 			success: function(results) {
 				// console.log(results);
 				controller.setResults(results);
